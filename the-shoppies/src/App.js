1d2dbbcb34fd0013ed057c5992a681e36aa92536
EirkinDahi <eirkin12@gmail.com>
2021-05-07 20:25:35 -0400

    styles for the app

--- the-shoppies/src/App.js
+++ the-shoppies/src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import Header from './components/Header';
+import Nominate from './components/Nominate';
+import Nominated from './components/Nominated';
+import RemoveNominated from './components/RemoveNominated'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function App(){
+  const[movies, setMovies] = useState([]);
+  const [searchValue, setSearchVal] = useState('');
+  const[nominated,setNominated] = useState([]);
+
+  const getMovieRequest = async(searchValue) =>{
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=8aace60e`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    
+    console.log(responseJson);
+
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  },[searchValue]);
+
+  // useEffect(()=>{
+  //   const movieNominated =JSON.parse(
+  //     localStorage.getItem('react-the-shoppies-nominated')
+  //   );
+  //   setNominated(movieNominated);
+  // },[]);
+
+  // const saveToLocal=(items)=>{
+  //   localStorage.setItem('react-the-shoppies-nominated',JSON.stringify(items));
+  // };
+
+  const nominateMovie =(movie)=>{
+    const nominateList =[...nominated,movie];
+    setNominated(nominateList);
+    // saveToLocal(nominateList);
+  };
+
+  const removeNominatedMovie=(movie)=>{
+    const nominateList=nominated.filter(
+      (nominate)=> nominate.imdbId !== movie.imdbId
+    );
+    setNominated(nominateList);
+    // saveToLocal(nominateList);
+  };
+
+  return(
+      <Router>
+        <Header/>
+        <Nominate searchValue = {searchValue} setSearchValue={setSearchVal}/>
+
+        <div className = 'container-fluid shoppies-app'>
+          <div className='row d-flex align-items-center'>
+            <MovieListHeading heading = 'Movies'/>
+          </div>
+        <div className ='row'>
+            <MovieList movies = {movies}  nominatedComponent={Nominated} handleNominateClick ={nominateMovie}/>
+        </div>
+        <div className='row d-flex align-items-center'>
+            <MovieListHeading heading = 'Nominated'/>
+          </div>
+        <div className ='row'>
+          <MovieList movies = {nominated} handleNominateClick={removeNominatedMovie} nominatedComponent={RemoveNominated} />
+        </div>
+       </div>
+
+      </Router>
   );
-}
+ 
+};
 
 export default App;
